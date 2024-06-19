06d4ee3fd496f664a1937a0036f4d0161f5b7277
Jenna Curls <98861047+jbug16@users.noreply.github.com>
2024-06-19 10:26:39 -0400

    fixed form validation and redirect

--- js/book-now.js
+++ js/book-now.js
@@ -1,24 +1,40 @@
-(function () {
+let form = document.querySelector('#bookingForm');
+    form.addEventListener('submit',(event)=>{
+        // Prevent submit
+        event.preventDefault();
+        // find inputs, textareas, and selects within the parent
+        let children = event.target.querySelectorAll('input, textarea, select');
+        // find if any of them are empty
+        let findEmpty = Array.from(children).find((element)=>{
+            if(element.value.length < 1){return true}
+            return false
+        });
+        // check if found an empty child
+        if(findEmpty){
+            
+            
+        }else{
+            // if not submit form
+            event.target.submit();
+        }
+    }
+);
+
+(() => {
     'use strict'
   
-    var forms = document.querySelectorAll('.needs-validation');
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
   
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
             if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-                console.log("Invalid.");
-            }
-            else 
-            {
-                console.log("Valid.");
-                window.location.replace("http://www.w3schools.com");
+            event.preventDefault()
+            event.stopPropagation()
             }
     
-            form.classList.add('was-validated');
-
+            form.classList.add('was-validated')
         }, false)
     })
 })()
