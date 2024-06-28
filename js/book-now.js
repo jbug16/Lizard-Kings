2ed9482fd1c77426561d189ce22d35dbaab2460a
Jenna Curls <98861047+jbug16@users.noreply.github.com>
2024-06-28 10:34:05 -0400

    added booking confirmation page

--- js/book-now.js
+++ js/book-now.js
@@ -5,7 +5,9 @@ const form = document.forms['contact-form'];
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
+
+  setTimeout(function() {
+    form.reset();
+    window.location.href = "other.html";
+  }, 500);
 })
